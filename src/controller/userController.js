98f83c449693b2d3d41e8fd981e59b8bd8741a79
Nimesh Dhumal <nimeshdhumal458@gmail.com>
2025-08-04 22:30:43 +0530

    update side code is completec.

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -4,7 +4,7 @@ exports.createSaver = async (req, res) => {
     if (!req.body.email || !req.body.firstName) {
         return res.status(400).send({ message: "First name and email are required!" });
     }
-    console.log(req.body);
+
     try {
         const user = await userModel.User.create({
             firstName: req.body.firstName,
@@ -29,17 +29,52 @@ exports.getAll = async (req, res) => {
 exports.getUserById = async (req, res) => {
     try {
         const id = req.params.id;
-        const userId = await userModel.User.findOne({ id: `${id}` });
-        res.send(userId);
+        const userId = await userModel.User.findOne({ where: { id: `${id}` } });
+        if (userId != null) {
+            res.send(userId);
+        } else {
+            res.status(400).send({ message: `DATA NOT FOUND BY GIVEN ID: ${id}` });
+        }
     } catch (error) {
         res.status(400).send({ message: "Error occurs:", error });
     }
 }
 
 exports.updateUser = async (req, res) => {
-    
+    const id = req.params.id;
+    if (!req.body.email || !req.body.firstName) {
+        return res.status(400).send({ message: "First name and email are required!" });
+    }
+
+    try {
+        const checkIdAvailability = await userModel.User.findOne({ where: { id: `${id}` } });
+
+        if (checkIdAvailability != null) {
+            try {
+                await userModel.User.update({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    email: req.body.email
+                }, { where: { id: `${id}` } });
+                res.status(201).send({ message: 'Data updated successfully!' });
+            } catch (error) {
+                res.status(500).send({ message: error.message || "Some error occurred while updating the User." });
+            }
+
+        } else { res.status(400).send({ message: `GIVEN ID DOES NOT FOUND :: ${req.params.id}` }); }
+    } catch (error) {
+        res.status(500).send({ message: error.message || "Some error occurred while updating the User." });
+    }
 }
 
 exports.deleteUser = async (req, res) => {
-
+    try {
+        const checkIdAvailability = await userModel.User.findOne({ where: { id: `${req.params.id}` } });
+        if (checkIdAvailability != null) {
+            const rowsAffected = await userModel.User.destroy({ where: { id: `${req.params.id}` } });
+            console.log(rowsAffected);
+        }
+    } catch (error) {
+        res.status(500).send({ message: error.message || "Some error occurred while deleting the User." });
+    }
 }
